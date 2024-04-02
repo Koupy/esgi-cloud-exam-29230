cbc6a76c290cca6ff11f05fee48b518ed9ef3da1
Koupy <nanook.tech@proton.me>
2024-04-02 12:31:45 +0200

    Update db.client.js

--- db.client.js
+++ db.client.js
@@ -1,18 +1,19 @@
-const { Sequelize } = require("sequelize");
-
-// database
 const sequelize = new Sequelize(
   process.env.DB_NAME, // Database name
   process.env.DB_USER, // User
   process.env.DB_PASSWORD, // Password
   {
+    host: process.env.DB_HOST, // Host
+    dialect: "postgres",
     dialectOptions: {
-      host: process.env.DB_HOST, // Host
-      dialect: "postgres",
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
+    },
+    define: {
+      createdAt: "added",
+      updatedAt: "updated",
     },
   }
 );
